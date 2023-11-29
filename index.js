749830a88530069eb34d14f90f1443fb00af0afe
Abhinav Kr Thakur <134839993+abhinavvthakur@users.noreply.github.com>
2023-11-29 23:12:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,15 +37,6 @@ bot.on('message', (msg) => {
     }
   });
 });
-const keepAlive = require('./server');
-const Monitor = require('ping-monitor');
-
-keepAlive();
-const monitor = new Monitor({
-    website: '',
-    title: 'NAME',
-    interval: 2
-});
 
 monitor.on('up', (res) => console.log(`${res.website} its on.`));
 monitor.on('down', (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
