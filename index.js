256e08bf9f1bfe76c1525a3a66996c227e3ff59a
Abhinav Kr Thakur <134839993+abhinavvthakur@users.noreply.github.com>
2023-11-29 05:38:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// index.js
+// main.js
 
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
-const xml2js = require('xml2js');
 
-const TOKEN = '6109622345:AAHoTVHfcVY11ImFbUqjvSwehfWNodAmEUc';
+const TOKEN = '5940303873:AAEq3kxtsDmOxmF4PEqqkjNMJVU-AcE8HuM';
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
@@ -15,36 +14,26 @@ bot.onText(/\/start/, (msg) => {
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const registrationNumber = msg.text;
+
+  // Check if the registrationNumber is provided
+  if (!registrationNumber || registrationNumber.trim() === "") {
+    bot.sendMessage(chatId, 'Please provide a valid vehicle registration number.');
+    return;
+  }
+
   const url = `https://api.royalsundaram.in/Services/CommonLookup/VehicleDetails?regn_no=${registrationNumber}`;
 
   request(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       try {
-        // Parse the XML content
-        xml2js.parseString(body, (parseError, result) => {
-          if (!parseError) {
-            // Beautify and send the JSON version of XML to the Telegram bot
-            const beautifiedXml = JSON.stringify(result, null, 2);
-            bot.sendMessage(chatId, `<pre>${beautifiedXml}</pre>`, { parse_mode: 'HTML' });
-          } else {
-            console.error('Error parsing XML:', parseError);
-            bot.sendMessage(chatId, 'Failed to retrieve vehicle details.');
-          }
-        });
+        // Print the XML content to the Telegram bot
+        bot.sendMessage(chatId, body, { parse_mode: 'HTML' });
       } catch (parseError) {
         console.error('Error parsing XML:', parseError);
         bot.sendMessage(chatId, 'Failed to retrieve vehicle details.');
       }
     } else {
-      try {
-        // Try to parse the error response as JSON
-        const errorResponse = JSON.parse(body);
-        const { statusCode, status, description } = errorResponse;
-        bot.sendMessage(chatId, `<b>Error:</b>\nStatus Code: ${statusCode}\nStatus: ${status}\nDescription: ${description}`, { parse_mode: 'HTML' });
-      } catch (jsonParseError) {
-        console.error('Error parsing JSON:', jsonParseError);
-        bot.sendMessage(chatId, 'Failed to retrieve vehicle details.');
-      }
+      bot.sendMessage(chatId, 'Failed to retrieve vehicle details.');
     }
   });
 });
